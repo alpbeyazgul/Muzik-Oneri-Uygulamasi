5fb4a63933f565221bc9faf0c0a6fbe7cd085450
Alp Beyazgül <alpbeyazgul@hotmail.com>
2023-05-31 00:05:31 +0300

    Create database.sql

---
+++ database/database.sql
@@ -0,0 +1,9 @@
+-- Veritabanı tablo yapısını buraya ekleyin
+-- Örnek olarak:
+-- CREATE TABLE muzikler (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     ad VARCHAR(255) NOT NULL,
+--     sanatci VARCHAR(255),
+--     album VARCHAR(255),
+--     tarih DATETIME DEFAULT CURRENT_TIMESTAMP
+-- );
